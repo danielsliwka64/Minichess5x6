3299e7eda166c34bbde1ebffcd25c14bb4540781
Daniel Śliwka <danielsliwkado@gmail.com>
2024-02-23 14:08:24 +0100

    Add project files.

---
+++ MiniChessLogic/Pieces/King.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChessLogic.Pieces
+{
+    public class King : Piece
+    {
+        public override PieceType Type => PieceType.King;
+        public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.Norh,
+            Direction.South,
+            Direction.West,
+            Direction.East,
+            Direction.SouthEast,
+            Direction.SouthWest,
+            Direction.NorthEast,
+            Direction.NorthWest
+        };
+        public King(Player color)
+        {
+            Color = color;
+        }
+    }
+}
